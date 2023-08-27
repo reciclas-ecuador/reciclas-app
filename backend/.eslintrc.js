feb5f7421d51324bc40eeb326cd26b72a603ca7f
Frankz Alarcon <leninalarcon7@gmail.com>
2023-08-27 10:34:27 -0500

    adding eslint rules and fixing conflicts

---
+++ backend/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": "standard-with-typescript",
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "project": "./tsconfig.json",
+        "tsconfigRootDir": __dirname,
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "ignorePatterns": [
+      "node_modules/",
+      "dist/",
+      ".eslintrc.js"
+    ],
+    "rules": {
+        // "no-tabs": ["error", { "allowIndentationTabs": true }],
+        "@typescript-eslint/space-before-function-paren": "off",
+    }
+}
