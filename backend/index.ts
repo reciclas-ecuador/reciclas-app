145add6a603f2280580bf5ac3ff7794e65b071c5
Frankz Alarcon <leninalarcon7@gmail.com>
2023-08-27 10:37:56 -0500

    creation of prisma models based on e-r diagram

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,15 @@
 import express from 'express'
 import cors from 'cors'
+import useRoutes from './routes'
 
 const app = express()
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 3000
 
 app.use(cors())
+app.use(express.json())
+
+useRoutes(app)
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+  console.log(`Server is running on port ${PORT}`)
 })
