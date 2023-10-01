723c8f13d2986b219b4da632f14399390bd97808
Rafael Parraga <angelzde36@gmail.com>
2023-09-30 23:02:30 -0500

    cambio local

--- frontend/assets/index.ts
+++ frontend/assets/index.ts
@@ -1,11 +1,9 @@
 import Home from './home.svg'
+import HomeActive from './homeActive.svg'
 
 export * from './general'
 export * from './login-signup'
 export * from './reception'
-<<<<<<< HEAD
 
-export { Home }
-=======
+export { Home, HomeActive }
 export * from './menu'
->>>>>>> 6e57cc4a6860a39a021d529f35e9670944772564
--- frontend/navigator/Navigator.tsx
+++ frontend/navigator/Navigator.tsx
@@ -3,7 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { UserMain_home } from '../user/components'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { NewsPage } from '../events/NewsPage'
-import { Home } from '../assets'
+import { Home, HomeActive } from '../assets'
 import DetailsNews from '../events/DetailsNews'
 import UserPage_home from '../user/UserPage_home'
 
@@ -18,18 +18,16 @@ const Navigator = () => {
         headerShown: false,
         tabBarLabelStyle: { display: 'none' },
         headerBackgroundContainerStyle: { borderColor: 'white' },
-        tabBarStyle: { backgroundColor: '#494D4F', borderColor: '#494D4F', height: 50 },
-        tabBarActiveBackgroundColor: 'green',
+        tabBarStyle: { backgroundColor: '#494D4f', borderColor: '#494D4F', height: 50, bottom: 10, marginHorizontal: 20, position: 'absolute', borderRadius: 60 },
         tabBarIcon: ({ focused, color, size }) => {
           let iconName: string
           if (route.name === 'UserMain_home') {
-            iconName = focused ? '/home.svg' : '/home.svg'
+            return focused ? <Home /> : <HomeActive width={30} height={30} />
           } else if (route.name === 'NewsPage') {
             iconName = focused ? '/assets/general/reciclasLogo.svg' : '/assets/general/reciclasLogo.svg'
           }
-          return <Home width={40} height={40} />
         },
-        tabBarActiveTintColor: '#494',
+        // tabBarActiveTintColor: '#494',
         tavBarInactiveTintColor: '#3e2465'
       })}
     >
@@ -39,35 +37,6 @@ const Navigator = () => {
       <Stack.Screen name='DetailsNews' component={DetailsNews} options={{ tabBarButton: () => null, tabBarVisible: false }} />
 
     </tab.Navigator>
-    // <Tab.Navigator
-    //   //   initialRouteName="Notas"
-    //   activeColor='#494'
-    //   //   labelStyle={{ fontSize: 12 }}
-    //   inactiveColor='#3e2465'
-    //   //   style={{ backgroundColor: "#c98804" }}
-    //   barStyle={{ backgroundColor: '#494D4F' }}
-    // >
-    //   <Tab.Screen
-    //     name='UserMain_home'
-    //     component={UserMain_home}
-    //     options={{
-    //       tabBarLabel: 'Home',
-    //       tabBarIcon: ({ color }) => (
-    //         <Image
-    //           source={require('../assets/home.svg')}
-    //           style={{ width: 30, height: 30 }}
-    //         />
-    //       )
-    //     }}
-    //   />
-    //   <Tab.Screen
-    //     name='User Qr'
-    //     component={UserGenerate_Qr}
-    //     options={{
-    //       tabBarLabel: 'QR'
-    //     }}
-    //   />
-    // </Tab.Navigator>
   )
 }
 
