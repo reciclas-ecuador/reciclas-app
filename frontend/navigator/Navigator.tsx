d7efd304dbb6b20b2d15e5319e0c1b9c882c6b29
DebianH <davidhernandez_s@hotmail.com>
2023-10-14 18:01:01 -0500

    Crea componente de ecoEqui en el usuario | Agrega navigation del usuario

--- frontend/navigator/Navigator.tsx
+++ frontend/navigator/Navigator.tsx
@@ -1,114 +1,175 @@
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { NewsPage } from '../events/NewsPage'
-import { Home, HomeActive, ReceptionCollectionCenter } from '../assets'
-import UserMain_home from '../user/components/User/UserMain_home'
-import TabNavigator from './TabNavigator'
-import { useRef } from 'react'
-import { Animated, Dimensions, View } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native'
-import { Ionicons } from '@expo/vector-icons'
-import { HomePageCollectionCenter, ReceptionPageCollectionCenter } from '../collection_center'
-import DetailsNews from '../events/DetailsNews'
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NewsPage } from "../events/NewsPage";
+import { Home, HomeActive, ReceptionCollectionCenter } from "../assets";
+import UserMain_home from "../user/components/User/UserMain_home";
+import UserQr_home from "../user/components/User/UserQr_home";
+import UserChart_home from "../user/components/User/UserChart_home";
+import TabNavigator from "./TabNavigator";
+import { useRef } from "react";
+import { Animated, Dimensions, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+import {
+  HomePageCollectionCenter,
+  ReceptionPageCollectionCenter,
+} from "../collection_center";
+import DetailsNews from "../events/DetailsNews";
 
-const Stack = createNativeStackNavigator()
-const tab = createBottomTabNavigator()
+const Stack = createNativeStackNavigator();
+const tab = createBottomTabNavigator();
 
 const Navigator = () => {
-  const tabOffsetValue = useRef(new Animated.Value(0)).current
+  const tabOffsetValue = useRef(new Animated.Value(0)).current;
 
-  console.log(tabOffsetValue)
+  console.log(tabOffsetValue);
 
   function getWidth() {
-    let width = Dimensions.get('window').width
+    let width = Dimensions.get("window").width;
     // total four Tabs...
-    return width / 4
+    return width / 5;
   }
+
+  interface TabIcon {
+    focused: string;
+    notFocused: string;
+  }
+
+  type TabIconMapping = Record<string, TabIcon>;
+
+  const tabIconMapping: TabIconMapping = {
+    UserMain_home: { focused: "home-outline", notFocused: "home" },
+    UserChart_home: {
+      focused: "bar-chart-outline",
+      notFocused: "bar-chart",
+    },
+    TabNavigator: { focused: "calendar-outline", notFocused: "calendar" },
+    HomePageCollectionCenter: {
+      focused: "bicycle-outline",
+      notFocused: "bicycle-sharp",
+    },
+    UserQr_home: { focused: "leaf-outline", notFocused: "leaf" },
+  };
 
   return (
     <NavigationContainer>
       <tab.Navigator
-        initialRouteName='TabNavigator'
+        initialRouteName="UserMain_home"
         screenOptions={({ route }) => ({
           headerShown: false,
-          tabBarLabelStyle: { display: 'none' },
+          tabBarLabelStyle: { display: "none" },
           tabBarStyle: {
-            backgroundColor: '#494D4f',
-            borderColor: '#494D4f'
+            backgroundColor: "#494D4f",
+            borderColor: "#494D4f",
           },
+          // tabBarIcon: ({ focused, color, size }) => {
+          //   let iconName: string;
+          //   if (route.name === "UserMain_home") {
+          //     iconName = !focused ? "home" : "home-outline";
+          //   } else if (route.name === "TabNavigator") {
+          //     iconName = !focused ? "calendar" : "calendar-outline";
+          //   } else if (route.name === "HomePageCollectionCenter") {
+          //     iconName = !focused ? "person" : "person-outline";
+          //   } else if (route.name === "UserQr_home") {
+          //     iconName = !focused ? "leaf" : "leaf-outline";
+          //   }
+          //   return <Ionicons name={iconName} size={size} color={color} />;
+          // },
+          // tabBarInactiveTintColor: "#494",
+          // tabBarActiveTintColor: "grey",
+
           tabBarIcon: ({ focused, color, size }) => {
-            let iconName: string
-            if (route.name === 'UserMain_home') {
-              iconName = !focused ? 'home' : 'home-outline'
-            } else if (route.name === 'TabNavigator') {
-              iconName = !focused ? 'calendar' : 'calendar-outline'
-            } else if (route.name === 'HomePageCollectionCenter') {
-              iconName = !focused ? 'person' : 'person-outline'
-            } else if (route.name === 'ReceptionPage') {
-              iconName = !focused ? 'leaf' : 'leaf-outline'
-            }
-            return <Ionicons name={iconName} size={size} color={color} />
+            const { focused: focusedIcon, notFocused: notFocusedIcon } =
+              tabIconMapping[route.name] || {};
+
+            return (
+              <Ionicons
+                name={focused ? focusedIcon : notFocusedIcon}
+                size={size}
+                color={color}
+              />
+            );
           },
-          tabBarInactiveTintColor: '#494',
-          tabBarActiveTintColor: 'grey'
+          tabBarInactiveTintColor: "#494",
+          tabBarActiveTintColor: "grey",
         })}
       >
-
-        <Stack.Screen name='UserMain_home' component={UserMain_home} listeners={({ navigation, route }) => ({
-          tabPress: () => {
-            Animated.spring(tabOffsetValue, {
-              toValue: 0,
-              useNativeDriver: true
-            }).start()
-          }
-        })}
+        <Stack.Screen
+          name="UserMain_home"
+          component={UserMain_home}
+          listeners={({ navigation, route }) => ({
+            tabPress: () => {
+              Animated.spring(tabOffsetValue, {
+                toValue: 0,
+                useNativeDriver: true,
+              }).start();
+            },
+          })}
         />
-        <Stack.Screen name='TabNavigator' component={TabNavigator} listeners={({ navigation, route }) => ({
-          tabPress: () => {
-            Animated.spring(tabOffsetValue, {
-              toValue: getWidth(),
-              useNativeDriver: true
-            }).start()
-          }
-        })}
+        <Stack.Screen
+          name="UserChart_home"
+          component={UserChart_home}
+          listeners={({ navigation, route }) => ({
+            tabPress: () => {
+              Animated.spring(tabOffsetValue, {
+                toValue: getWidth(),
+                useNativeDriver: true,
+              }).start();
+            },
+          })}
         />
-        <Stack.Screen name='HomePageCollectionCenter' component={HomePageCollectionCenter} listeners={() => ({
-          tabPress: () => {
-            Animated.spring(tabOffsetValue, {
-              toValue: getWidth() * 2,
-              useNativeDriver: true
-            }).start()
-          }
-        })}
+        <Stack.Screen
+          name="TabNavigator"
+          component={TabNavigator}
+          listeners={({ navigation, route }) => ({
+            tabPress: () => {
+              Animated.spring(tabOffsetValue, {
+                toValue: getWidth() * 2,
+                useNativeDriver: true,
+              }).start();
+            },
+          })}
         />
-        <Stack.Screen name='ReceptionPage' component={ReceptionPageCollectionCenter} listeners={() => ({
-          tabPress: () => {
-            Animated.spring(tabOffsetValue, {
-              toValue: getWidth() * 3,
-              useNativeDriver: true
-            }).start()
-          }
-        })}
+        <Stack.Screen
+          name="HomePageCollectionCenter"
+          component={HomePageCollectionCenter}
+          listeners={() => ({
+            tabPress: () => {
+              Animated.spring(tabOffsetValue, {
+                toValue: getWidth() * 3,
+                useNativeDriver: true,
+              }).start();
+            },
+          })}
         />
-
+        <Stack.Screen
+          name="UserQr_home"
+          component={UserQr_home}
+          listeners={() => ({
+            tabPress: () => {
+              Animated.spring(tabOffsetValue, {
+                toValue: getWidth() * 4,
+                useNativeDriver: true,
+              }).start();
+            },
+          })}
+        />
       </tab.Navigator>
       <Animated.View
         style={{
           width: 50,
           height: 4,
-          position: 'absolute',
-          backgroundColor: '#494',
+          position: "absolute",
+          backgroundColor: "#494",
           bottom: 45,
-          left: 23,
+          left: 14,
           borderRadius: 50,
-          transform: [
-            { translateX: tabOffsetValue }
-          ]
+          transform: [{ translateX: tabOffsetValue }],
         }}
       />
     </NavigationContainer>
-  )
-}
+  );
+};
 
-export default Navigator
+export default Navigator;
--- frontend/user/components/User/UserMain_home.tsx
+++ frontend/user/components/User/UserMain_home.tsx
@@ -1,11 +1,53 @@
 import { Gradient } from "../../../global";
 import React, { useEffect, useState } from "react";
-import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
+import { View, Image, StyleSheet, Dimensions } from "react-native";
 import { getToDataUser } from "../../services/user_service";
 import { Data } from "../../../Types";
+import {
+  AntDesign,
+  MaterialCommunityIcons,
+  MaterialIcons,
+} from "@expo/vector-icons";
+// import { MaterialCommunityIcons } from '@expo/vector-icons';
+import {
+  Modal,
+  Portal,
+  Text,
+  Button,
+  PaperProvider,
+  TextInput,
+} from "react-native-paper";
+import { ProgressChart } from "react-native-chart-kit";
+
+const screenWidth = Dimensions.get("window").width - 60;
+const chartConfig = {
+  backgroundGradientFrom: "#bdf26d",
+  backgroundGradientFromOpacity: 0,
+  backgroundGradientTo: "#494",
+  backgroundGradientToOpacity: 0.5,
+  color: (opacity = 1) => `rgba(255, 255, 150, ${opacity})`,
+  strokeWidth: 2, // optional, default 3
+  barPercentage: 0.5,
+  useShadowColorFromDataset: false, // optional
+};
+const data = {
+  labels: ["Medalla", "Dias", "Kg"], // optional
+  data: [0.8, 0.12, 0.013],
+};
 
 const UserMain_home = () => {
   const [dataUser, setDataUser] = useState<Data[]>();
+  const [visible, setVisible] = React.useState(false);
+  const [text, setText] = React.useState("");
+
+  const showModal = () => setVisible(true);
+  const hideModal = () => setVisible(false);
+  const containerStyle = {
+    backgroundColor: "white",
+    padding: 30,
+    margin: 20,
+    borderRadius: 10,
+  };
 
   async function fetchData() {
     const dataUserBasic = await getToDataUser();
@@ -16,42 +58,157 @@ const UserMain_home = () => {
     fetchData();
   }, []);
   return (
-    <Gradient>
-      <View style={styles.box}>
-        {dataUser?.map((item) => (
-          <View style={styles.contentData} key={item.id}>
-            <View>
-              <Image
-                style={{ width: 100, height: 100, borderRadius: 50 }}
-                // source={{ uri: item.url }}
-                source={{
-                  uri: "https://png.pngtree.com/png-vector/20210426/ourlarge/pngtree-young-man-cartoon-profile-vector-hd-image-png-image_3238138.jpg",
-                }}
-              />
-            </View>
+    <PaperProvider>
+      <Gradient>
+        <View style={styles.box}>
+          <AntDesign
+            name="ellipsis1"
+            size={24}
+            color="#c0c0c0"
+            style={{ zIndex: 2, position: "absolute", top: 5, right: 15 }}
+            onPress={showModal}
+          />
+          {dataUser?.map((item) => (
+            <View style={styles.contentData} key={item.id}>
+              <View>
+                <Image
+                  style={{ width: 100, height: 100, borderRadius: 50 }}
+                  // source={{ uri: item.url }}
+                  source={{
+                    uri: "https://png.pngtree.com/png-vector/20210426/ourlarge/pngtree-young-man-cartoon-profile-vector-hd-image-png-image_3238138.jpg",
+                  }}
+                />
+              </View>
 
-            <View>
-              <Text style={styles.text}>Hola! {item.title.split(" ")[0]}</Text>
+              <View>
+                <Text style={styles.text}>
+                  Hola! {item.title.split(" ")[0]}
+                </Text>
+              </View>
+            </View>
+          ))}
+          <View>
+            <Text variant="titleLarge" style={styles.textMain}>
+              Bienvenidos a Reciclas la mejor App de reciclaje.
+            </Text>
+          </View>
+        </View>
+        {/* ----- */}
+
+        <View style={styles.prueba}>
+          <View>
+            <Text
+              variant="titleLarge"
+              style={{
+                color: "white",
+                paddingVertical: 10,
+                textAlign: "center",
+                // display: "flex",
+                // alignItems: "center",
+                // justifyContent: "center",
+                // alignContent: "center",
+              }}
+            >
+              <MaterialCommunityIcons
+                name="progress-star"
+                size={24}
+                color="white"
+              />
+              TU PROGRESO
+            </Text>
+            {/* <Text style={{ color: "white", zIndex: 5 }}>
+              {JSON.stringify(dataUser)}
+            </Text> */}
+
+            <ProgressChart
+              data={data}
+              width={screenWidth}
+              height={220}
+              strokeWidth={16}
+              radius={32}
+              chartConfig={chartConfig}
+              hideLegend={false}
+            />
+          </View>
+        </View>
+        {/* </ScrollView> */}
+      </Gradient>
+      {/* -------- Modal -------- */}
+      <Portal>
+        <Modal
+          visible={visible}
+          onDismiss={hideModal}
+          contentContainerStyle={containerStyle}
+        >
+          <View>
+            <Text variant="headlineSmall">
+              <MaterialIcons name="person-pin" size={20} color="black" /> Mi
+              Perfil
+            </Text>
+            <TextInput
+              mode="outlined"
+              label="Nombre"
+              placeholder="Type something"
+              right={<TextInput.Affix text="/100" />}
+            />
+            <TextInput
+              mode="outlined"
+              label="Apellido"
+              placeholder="Type something"
+              right={<TextInput.Affix text="/100" />}
+            />
+
+            <TextInput
+              mode="outlined"
+              label="Correo"
+              placeholder="Type something"
+              right={<TextInput.Affix text="/40" />}
+            />
+
+            <View
+              style={{
+                marginTop: 20,
+                alignItems: "center",
+              }}
+            >
+              <Text variant="bodyMedium">
+                <MaterialCommunityIcons
+                  name="face-agent"
+                  size={24}
+                  color="black"
+                />{" "}
+                Contacta a soporte
+              </Text>
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                display: "flex",
+                justifyContent: "space-around",
+                alignItems: "center",
+                marginTop: 20,
+              }}
+            >
+              <Button
+                icon="account-edit"
+                mode="outlined"
+                style={{ borderColor: "#bdf26d" }}
+                onPress={() => console.log("Pressed")}
+              >
+                Actualizar
+              </Button>
+              <Button
+                icon="blur-off"
+                style={{ backgroundColor: "#bdf26d" }}
+                onPress={() => hideModal()}
+              >
+                Salir
+              </Button>
             </View>
           </View>
-        ))}
-        <View>
-          <Text style={styles.textMain}>
-            Bienvenidos a Reciclas la mejor App de reciclaje.
-          </Text>
-        </View>
-      </View>
-      {/* ----- */}
-
-      <View style={styles.prueba}>
-        <View>
-          <Text style={{ color: "white", zIndex: 5 }}>
-            {JSON.stringify(dataUser)}
-          </Text>
-        </View>
-      </View>
-      {/* </ScrollView> */}
-    </Gradient>
+        </Modal>
+      </Portal>
+    </PaperProvider>
   );
 };
 
@@ -102,7 +259,7 @@ const styles = StyleSheet.create({
   prueba: {
     flex: 1,
     width: "90%",
-    height: "58%",
+    height: "55%",
     position: "absolute",
     bottom: 15,
     borderRadius: 20,
