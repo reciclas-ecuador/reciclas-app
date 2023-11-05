22e76c68791f081aa63a96f686cad0e879d1b5bd
Rafael Parraga <angelzde36@gmail.com>
2023-11-05 16:38:58 -0500

    add cohere chat and fixed maps

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,9 +1,5 @@
-
-// import { SignupPage } from './collection_center/SignupPage'
-import { NewsPage } from "./events/NewsPage";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
-import Navigator from "./navigator/Navigator";
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import Navigator from './navigator/Navigator'
 
 // *** Luego de probar sus views por favor borrar antes de subir ***
 export default function App() {
--- frontend/cohere/services/cohereGenerate.ts
+++ frontend/cohere/services/cohereGenerate.ts
@@ -1,18 +1,21 @@
-const cohere = require('cohere-ai');
-cohere.init('tPgdL0VP7EbnQiEkymbVuZto6DQlGHDNzq3HnYTh'); 
+import { CohereClient } from 'cohere-ai'
 
-export const cohereGenerate = async (inputs: String): Promise<String> => {
+const cohere = new CohereClient({
+  token: 'tPgdL0VP7EbnQiEkymbVuZto6DQlGHDNzq3HnYTh'
+})
+
+export const cohereGenerate = async (inputs: string): Promise<string> => {
   const response = await cohere.generate({
     model: 'command',
     prompt: `You are a "recycling" and "cycling" expert willing to answer questions
     about best practices in recycling, waste management, biodiversity conservation or advices 
     to start cycling and any other topic related to the environment and cycling. Answer the following question: ${inputs}.
     Provide a short, precise and useful answer in "spanish".`,
-    temperature:0.3, 
-    k :72, 
-    p:0.9
-    }
-  );
+    temperature: 0.3,
+    k: 72,
+    p: 0.9
+  }
+  )
 
-  return response.body.generations[0].text;
-};
+  return response.generations[0].text
+}
