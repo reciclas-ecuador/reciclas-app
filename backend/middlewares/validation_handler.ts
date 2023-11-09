84abe3fad90f18063f662c16fd10b792e138f543
Frankz Alarcon <leninalarcon7@gmail.com>
2023-11-09 07:34:54 -0500

    fixing bugs to deploy

--- backend/middlewares/validation_handler.ts
+++ backend/middlewares/validation_handler.ts
@@ -2,7 +2,7 @@ import boom from '@hapi/boom'
 import { type NextFunction, type Request, type Response } from 'express'
 import { type ObjectSchema } from 'joi'
 import { type Role } from '../src/auth/types/auth'
-// import AuthService from '../src/auth/services/auth_service'
+import AuthService from '../src/auth/services/auth_service'
 
 export const validationHandler = (schema: ObjectSchema, property: 'body' | 'params') => {
   return (req: Request, _res: Response, next: NextFunction) => {
@@ -20,19 +20,18 @@ export const validationHandler = (schema: ObjectSchema, property: 'body' | 'para
 export const checkTokenAndRoles = (allowedRoles: Role[]) => {
   return async (req: Request, _res: Response, next: NextFunction) => {
     try {
-      // const token = req.headers.authorization?.split(' ')[1]
-      // console.log('[token]', token)
-      // if (token === undefined) {
-      //   next(boom.unauthorized('Token not found'))
-      //   return
-      // }
-      // const decodedToken = await new AuthService().verifyIdToken(token)
-      // console.log(decodedToken)
-      // if (!allowedRoles.includes(decodedToken.role)) {
-      //   next(boom.forbidden('User has no permissions'))
-      //   return
-      // }
-      // req.user = decodedToken
+      const token = req.headers.authorization?.split(' ')[1]
+      if (token === undefined) {
+        next(boom.unauthorized('Token not found'))
+        return
+      }
+      const decodedToken = await new AuthService().verifyIdToken(token)
+      console.log(decodedToken)
+      if (!allowedRoles.includes(decodedToken.role)) {
+        next(boom.forbidden('User has no permissions'))
+        return
+      }
+      req.user = decodedToken
       next()
     } catch (error) {
       next(error)
